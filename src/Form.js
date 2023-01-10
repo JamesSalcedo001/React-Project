4bee60f08941a72c0e87374c3290e87f757af59b
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-01-10 12:27:08 -0600

    added a post request when submit event occurs, also made form controlled with state

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,39 @@
+import { useState} from "react"
+
 function Form({coffeeOrder}) {
 
+    const [formData, setFormData] = useState({
+        name: "",
+        type: "",
+        recipe: "",
+        image: "",
+    })
 
 
     function submit(e) {
         e.preventDefault()
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({...formData, likes: 0}),
+        }
+        fetch("http://localhost:3000/products", configObj)
+        .then((res) => res.json())
+        .then((coffee) => {
+            coffeeOrder(coffee)
+            setFormData({
+                name: "",
+                type: "",
+                recipe: "",
+                image: "",
+            })
+        })
     }
+
+
 
     return (
         <div>
@@ -12,16 +41,16 @@ function Form({coffeeOrder}) {
                 <form onSubmit={submit}>
                     <h3>Mobile Order!</h3>
                     <label htmlFor="name">Name</label>
-                    <input type="text" name="name" placeholder="what coffee?"/>
+                    <input type="text" name="name" value={formData.name} placeholder="what coffee?"/>
 
                     <label htmlFor="type">Type</label>
-                    <input type="text" placeholder="blended, brewed, espresso, tea, iced coffee, other?"/>
+                    <input type="text" value={formData.type} placeholder="blended, brewed, espresso, tea, iced coffee, other?"/>
 
                     <label htmlFor="recipe">Type</label>
-                    <input type="text" placeholder="size, description, inclusions, exclusions?"/>
+                    <input type="text" value={formData.recipe} placeholder="size, description, inclusions, exclusions?"/>
 
                     <label htmlFor="image">Type</label>
-                    <input type="text" placeholder="what does it look like?"/>
+                    <input type="text" value={formData.image} placeholder="what does it look like?"/>
 
                     <input type="submit" value="Order!"/>
                 </form>
