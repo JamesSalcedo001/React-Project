a32f0db6bb1ff059eb30f0ccac897311fc533c55
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-01-10 10:12:00 -0600

    next, I imported and added state and used a state setter function to set the state to the data I recieve with fetch

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import {Switch, Route} from "react-router-dom"
 import Form from "./Form"
 import Home from "./Home"
 import List from "./List"
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
 
 
 
 function App() {
 
+  const [coffee, setCoffee] = useState([])
+
   useEffect(() => {
     fetch("http://localhost:3000/products")
     .then((res) => res.json())
-    .then(console.log)
+    .then((coffee) => setCoffee(coffee))
   },[])
-  
+
   return (
     <div className="App">
       <Header/>
