523e37f4364d46a0210448a1527e4210da8cea31
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-01-10 11:55:35 -0600

    added a clock component to my home component to display the current time every second

---
+++ src/Clock.js
@@ -0,0 +1,22 @@
+import {useState, useEffect} from "react"
+
+function Clock() {
+    const [date, setDate] = useState(new Date())
+
+    function clockChange() {
+        setDate(new Date())
+    }
+
+    useEffect(() => {
+        const timer = setInterval(clockChange, 1000)
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    }, [])
+
+    return (
+        <span> {date.toLocaleTimeString()}</span>
+    )
+}
+
+export default Clock;
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
+import Clock from "./Clock"
+
 function Home() {
     return (
         <div>
             <h1>HOMEPAGE</h1>
             <p>My Name is James Salcedo and I am an Aspiring Software Engineer! Welcome to My Site!</p>
+            { "The Time is Now: "}<Clock/>
         </div>
     )
 }
